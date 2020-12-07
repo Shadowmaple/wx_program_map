8a9a06f7dde1f9bacf3df0f18a5b7ec347f133bd
Shadowmaple <1142319190@qq.com>
2020-12-08 00:28:47 +0800

    complete; add position item and bind function

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,4 @@
 // 微信小程序查看附近的餐厅
-//index.js
-//获取应用实例
-// const app = getApp()
 
 // 引入SDK核心类
 var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
@@ -14,15 +11,8 @@ var centerMarker = {
   id: 0,
   latitude: 23.096994,
   longitude: 113.324520,
-  width: 30,
-  height: 50,
-}
-
-var foodMarker = {
-  iconPath: '../../images/restaurant.png',
-  id: 0,
-  latitude: 23.096994,
-  longitude: 113.324520,
+  width: 40,
+  height: 40,
 }
 
 Page({
@@ -33,26 +23,21 @@ Page({
     maph: '0',
     scale: '16',
     markers: [centerMarker],
-    // markers: [{
-    //   iconPath: '../../images/map.png',
-    //   id: 0,
-    //   latitude: 23.096994,
-    //   longitude: 113.324520,   
-    // }]
   },
+  mapCtx: null,
   onLoad: function () {
     // 实例化API核心类
     qqmapsdk = new QQMapWX({
       key: key
     })
+    this.mapCtx = wx.createMapContext('map', this)
     wx.getSystemInfo({
       success: res => {
-        console.log(res.windowWidth)
-        console.log(res.windowHeight)
         var mapw = res.windowWidth
         var maph = res.windowHeight
+        console.log(maph)
         this.setData({
-          maph: maph + 'px'
+          maph: maph + 'px',
         })
       }
     })
@@ -75,15 +60,20 @@ Page({
       location: {
         longitude: longitude,
         latitude: latitude,
-      },    
+      },
+      page_size: 20,
       success: res => {
         console.log('search from map successfully', res);
         var marks = []
         for (let i in res.data) {
-          foodMarker.id = i + 1
-          foodMarker.longitude = res.data[i].location.lng,
-            foodMarker.latitude = res.data[i].location.lat,
-            marks.push(foodMarker)
+          marks.push({
+            iconPath: '../../images/restaurant.png',
+            id: Number(i + 1),
+            latitude: res.data[i].location.lat,
+            longitude: res.data[i].location.lng,
+            width: 30,
+            height: 30,
+          })
         }
         centerMarker.longitude = longitude
         centerMarker.latitude = latitude
@@ -96,64 +86,20 @@ Page({
       fail: function (res) {
         console.log(res);
       },
-      // complete: function (res) {
-      //   console.log('complete', res);
-      // }
     })
   },
-  onShow: function () {
-    // 调用接口
-    // qqmapsdk.search({})
+  bindControlTap: function (e) {
+    console.log("move to center")
+    this.mapCtx.moveToLocation()
+  },
+  bindReginChange: function (e) {
+    console.log("e")
+    if (e.type == 'end') {
+      this.mapCtx.getCenterLocation({
+        success: res => {
+          this.getFood(res.longitude, res.latitude)
+        }
+      })
+    }
   },
 })
-
-// Page({
-//   data: {
-//     motto: 'Hello World',
-//     userInfo: {},
-//     hasUserInfo: false,
-//     canIUse: wx.canIUse('button.open-type.getUserInfo')
-//   },
-//   //事件处理函数
-//   bindViewTap: function() {
-//     wx.navigateTo({
-//       url: '../logs/logs'
-//     })
-//   },
-//   onLoad: function () {
-//     if (app.globalData.userInfo) {
-//       this.setData({
-//         userInfo: app.globalData.userInfo,
-//         hasUserInfo: true
-//       })
-//     } else if (this.data.canIUse){
-//       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-//       // 所以此处加入 callback 以防止这种情况
-//       app.userInfoReadyCallback = res => {
-//         this.setData({
-//           userInfo: res.userInfo,
-//           hasUserInfo: true
-//         })
-//       }
-//     } else {
-//       // 在没有 open-type=getUserInfo 版本的兼容处理
-//       wx.getUserInfo({
-//         success: res => {
-//           app.globalData.userInfo = res.userInfo
-//           this.setData({
-//             userInfo: res.userInfo,
-//             hasUserInfo: true
-//           })
-//         }
-//       })
-//     }
-//   },
-//   getUserInfo: function(e) {
-//     console.log(e)
-//     app.globalData.userInfo = e.detail.userInfo
-//     this.setData({
-//       userInfo: e.detail.userInfo,
-//       hasUserInfo: true
-//     })
-//   }
-// })
